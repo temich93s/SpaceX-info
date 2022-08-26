894676b35d9573ddb4c555f9699285cfe4a06623
Temich93s <nil>
2022-08-27 09:45:09 +1000

    Initial Commit

---
+++ SpaceX-info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX-info
+//
+//  Created by 2lup on 27.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
