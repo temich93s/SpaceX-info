1d402f47bafdd35c9f0b85fd298bf9b8713c6967
Temich93s <nil>
2022-08-27 10:57:23 +1000

    Create PageViewController

---
+++ SpaceX-info/PageViewController.swift
@@ -0,0 +1,60 @@
+//
+//  PageViewController.swift
+//  SpaceX-info
+//
+//  Created by 2lup on 27.08.2022.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // устанавливаем себя источником данных для UIPageViewControllerDataSource
+        self.dataSource = self
+        
+        // заполняем значение по умолчанию передавая массив с одним ViewController который отобразится по умолчанию (setViewControllers можно добавить комплишнхендлер)
+        if let vc = self.createPageViewController(for: 0) {
+            setViewControllers([vc], direction: .forward, animated: true)
+        }
+    }
+}
+
+// MARK: - UIPageViewControllerDataSource
+
+extension PageViewController: UIPageViewControllerDataSource {
+    
+    // viewControllerBefore - возвращаем предыдущий ViewController
+    // return nil - означает, что страница предыдущая/следующая не существует
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        // получаем индекс предыдущего ViewController
+        let index = ((viewController as? ViewController)?.index ?? 0) - 1
+        return createPageViewController(for: index)
+    }
+
+    // viewControllerBefore - возвращаем следующуй ViewController
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        // получаем индекс следующего ViewController
+        let index = ((viewController as? ViewController)?.index ?? 0) + 1
+        return createPageViewController(for: index)
+    }
+    
+    // метод генерирующий ViewController для определенного индекса
+    func createPageViewController(for index: Int) -> UIViewController? {
+        // проверяем что бы мы не вышли за пределы количества ViewController
+        if (index < 0) || (index > 3) {
+            return nil
+        } else {
+            // instantiateViewController - Создает ViewController с указанным идентификатором (storyboard ID) и инициализирует его данными из storyboard
+            // И кастим результат до нашего кастомного ViewController
+            let vc = storyboard?.instantiateViewController(withIdentifier: "oneNewsSID") as? ViewController
+            vc?.index = index
+            return vc
+        }
+        
+
+    }
+}
+
--- SpaceX-info/ViewController.swift
+++ SpaceX-info/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        switch index {
+        case 0: view.backgroundColor = UIColor.blue
+        case 1: view.backgroundColor = UIColor.green
+        case 2: view.backgroundColor = UIColor.yellow
+        case 3: view.backgroundColor = UIColor.orange
+        default: view.backgroundColor = UIColor.gray
+        }
     }
 
 
